ddf7949ad3670a5704949ff4607f1a7908bd96f9
PROTOKATANA <PROTOKATANA@proton.me>
2026-04-12 21:08:10 +0200

    +-+-+-+-+

--- bebe/bandeja/locales/en.js
+++ bebe/bandeja/locales/en.js
@@ -1,6 +1,11 @@
 (function () {
     window.EcoNichoLocales = window.EcoNichoLocales || {};
     window.EcoNichoLocales.en = {
+        scb_perk_discount: '30% off',
+        scb_perk_gift: '1 surprise gift',
+        scb_perk_ends: 'Ends in',
+        sticky_cart_btn: 'CART',
+        sticky_pay_btn: 'PAY',
         title_product:  '4-Pack Chew Toys for Dogs - BabyNest',
         product_title:  '4-Pack Chew Toys for Dogs',
         product_desc:   'The perfect pack to keep your dog entertained and protect your furniture. Made from resistant natural rubber, safe for all breeds. Includes 4 different models to stimulate your pet\'s natural instincts.',
--- bebe/bandeja/locales/es.js
+++ bebe/bandeja/locales/es.js
@@ -1,6 +1,11 @@
 (function () {
     window.EcoNichoLocales = window.EcoNichoLocales || {};
     window.EcoNichoLocales.es = {
+        scb_perk_discount: '30% descuento',
+        scb_perk_gift: '1 regalo sorpresa',
+        scb_perk_ends: 'Termina en',
+        sticky_cart_btn: 'CARRITO',
+        sticky_pay_btn: 'PAGAR',
         title_product:  'Pack 4 Juguetes Masticables - BabyNest',
         product_title:  'Pack 4 Juguetes Masticables para Perros',
         product_desc:   'El pack perfecto para mantener a tu perro entretenido y proteger tus muebles. Fabricados en goma natural resistente, seguros para todas las razas. Incluye 4 modelos diferentes para estimular el instinto natural de tu mascota.',
